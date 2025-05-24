8b1fc384be52d660bdfb8edf57b6e845a37043ff
Oscar Mauricio Rivera <omrs0879@gmail.com>
2025-05-24 11:24:13 -0500

    Cambio version

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,7 +7,7 @@
           <q-icon name="menu" />
         </q-btn>
         <q-toolbar-title>
-          SILAMED V 2024.11.16.1
+          SILAMED V 2025.05.19.1
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
