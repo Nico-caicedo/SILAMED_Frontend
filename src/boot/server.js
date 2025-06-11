a28dca930e7c4a39155b884647bb62e0441ea13e
Oscar Mauricio Rivera <omrs0879@gmail.com>
2025-06-11 08:27:12 -0500

    cambios breves

--- src/boot/server.js
+++ src/boot/server.js
@@ -5,7 +5,7 @@
 // const UrlServer = "http://170.0.0.21:3000/"
 //const UrlServer = "http://192.168.1.103/SILAMED/imgs/"
 // const UrlServer = "http://192.168.108.2/imgs/"
-const UrlServer = "http://170.0.0.19/imgs/"  
+const UrlServer = "http://170.0.0.19/SILAMED/ImagenesServidor/"  
 
 // const UrlServer = "http://192.168.0.109:3000/"
 // const UrlServer = "http://192.168.210.2/SILAMED/imgs/"
